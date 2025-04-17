b623d67913b4dbd77ef7402ab82a5952ff54ed3d
Sarah Lean <13692824+weeyin83@users.noreply.github.com>
2025-04-17 12:47:24 +0100

    Create variables.tf

---
+++ key-vault-deployment/variables.tf
@@ -0,0 +1,21 @@
+##
+# Variables
+##
+
+##
+# Common Variables
+##
+variable "tag_environment" {
+  type    = string
+  default = "Testing"
+}
+
+variable "tag_project" {
+  type    = string
+  default = "KeyVault"
+}
+
+variable "tag_creator" {
+  type    = string
+  default = "TechieLass"
+}
